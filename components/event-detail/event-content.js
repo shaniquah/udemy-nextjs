96dc63f9354aaf2f271ca40967885f7660ead767
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-09-20 10:08:03 +0200

    docs: create new branch for Section 8 code along

--- components/event-detail/event-content.js
+++
@@ -1,9 +0,0 @@
-import classes from "./event-content.module.css";
-
-export default function EventContent(props) {
-  return (
-  <section className={classes.content}>
-    {props.children}
-  </section>
-  )
-}
--- components/ui/error-alert.js
+++
@@ -1,5 +0,0 @@
-import classes from "./error-alert.module.css";
-
-export default function ErrorAlert(props) {
-  return <div className={classes.alert}>{props.children}</div>;
-}
